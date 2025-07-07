f139585cedc7145ce9ad4949f06ea2d83d4e3f81
i7C-40 <i7C-40@DESKTOP-4JEE2Q3>
2025-07-07 16:58:21 +0900

    버튼

---
+++ src/insurance/SeoButton.java
@@ -0,0 +1,11 @@
+package insurance;
+
+import javax.swing.JButton;
+
+public class SeoButton extends JButton{
+	public static void main(String[] args) {
+		JButton nb = new JButton();
+		nb.setActionCommand("꾸욱~");
+		nb.doClick();
+	}
+}
