0a0bc7305dd5a4e08ae013e08968974005e62474
Si7C-08 <Si7C-08@i7C-85>
2025-07-07 16:51:44 +0900

    청구 메인 콘솔

--- src/insurance/claim/claimMainConsole.java
+++ src/insurance/claim/claimMainConsole.java
@@ -2,6 +2,6 @@ package insurance.claim;
 
 public class claimMainConsole {
 	public static void main(String[] args) {
-		System.out.println("청구 메인 콘솔 입니다!!");
+		System.out.println("청구 메인 콘솔 입니다!!!");
 	}
 }
