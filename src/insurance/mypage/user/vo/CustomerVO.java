bd02917bb0ee6662d02e5c2b4e466a8b3750291a
Si7C-08 <Si7C-08@Si7A-39>
2025-07-09 16:52:15 +0900

    초기 클래스 생성

---
+++ src/insurance/mypage/user/vo/CustomerVO.java
@@ -0,0 +1,24 @@
+package insurance.mypage.user.vo;
+
+public class CustomerVO {
+	private String customer_id;
+	private String customer_name;
+	private String personal_id;
+	private String email;
+	private String zip_code;
+	private String address_1;
+	private String address_2;
+	private String phone_number;
+	private String job;
+	private String company_name;
+	private String job_zip_code;
+	private String job_address1;
+	private String job_address2;
+	private String job_phone_number;
+	private String agree_yn;
+	private String login_id;
+	private String password_hash;
+	private String password_salt;
+	private String created_at;
+
+}
