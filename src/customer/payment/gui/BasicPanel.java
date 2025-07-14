7d0c335df5795dedc7289dc657e3e7b9308fb3a8
i7C-40 <i7C-40@DESKTOP-4JEE2Q3>
2025-07-14 17:23:45 +0900

    서호은 커밋

---
+++ src/customer/payment/gui/BasicPanel.java
@@ -0,0 +1,20 @@
+package customer.payment.gui;
+
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Dimension;
+
+public class BasicPanel extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Create the panel.
+	 */
+	public BasicPanel() {
+		setBackground(new Color(255, 255, 255));
+		setLayout(null);
+		setPreferredSize(new Dimension(1440, 700));
+	}
+
+}
