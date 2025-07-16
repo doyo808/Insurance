5923f8588101276bd2bdee1fb9226550863c7dd5
Si7C-08 <Si7C-08@Si7A-30>
2025-07-16 17:08:51 +0900

    상품직원페이지 기본 틀잡기

--- src/employee/product/view/EditPanel.java
+++ src/employee/product/view/EditPanel.java
@@ -1,7 +1,18 @@
 package employee.product.view;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JPanel;
+
+import employee.product.view.buttons.EditPanelBottom;
+import employee.product.view.center.EditPanelCenter;
 
 public class EditPanel extends JPanel{
 	
+	public EditPanelCenter center;
+	public EditPanelBottom bottom;
+	
+	public EditPanel() {
+		setLayout(new BorderLayout());
+	}
 }
